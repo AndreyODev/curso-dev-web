6d41c28c01508b00785623eb69f1c8ad207bfb0d
Andrey Oliveira <andreyoliveira72005@gmail.com>
2025-12-22 12:57:59 -0300

    feat: atualiza arquivos de estudo em JavaScript

--- javascript/arquivos-js/variavel.js
+++ javascript/arquivos-js/variavel.js
@@ -1,31 +1,55 @@
-// Declaração de variáveis com var
-var a, b, c;
+// // Declaração de variáveis com var
+// var a, b, c;
 
-var nome, sobrenome, nomeCompleto, idade;
-// Atribuição de valores
-a = 2;
-b = 3;
-c = a + b;
-// alert(c);
+// var nome, sobrenome, nomeCompleto, idade;
+// // Atribuição de valores
+// a = 2;
+// b = 3;
+// c = a + b;
+// // alert(c);
 
-nome = "Andrey";
-sobrenome = "Oliveira Nunes";
-idade = 19;
-nomeCompleto = nome + " " + sobrenome;
+// nome = "Andrey";
+// sobrenome = "Oliveira Nunes";
+// idade = 19;
+// nomeCompleto = nome + " " + sobrenome;
 
-pessoa = nome + " " + idade;
-document.getElementById("texto").innerHTML = pessoa;
+// pessoa = nome + " " + idade;
+// document.getElementById("texto").innerHTML = pessoa;
 
-// Declaração de variáveis com let
-let x = 10;
+// // Declaração de variáveis com let
+// let x = 10;
 
-{
-	let x = 2;
+// {
+// 	let x = 2;
+// }
+
+// // document.getElementById("texto").innerHTML = x;
+
+// // Declaração de variável com const
+// const y = 5;
+
+// document.getElementById("texto").innerHTML = y;
+
+let horario = 18;
+// alert(horario);
+
+let saudacao = "";
+
+if (horario < 12) {
+	saudacao = "bom dia";
+} else if (horario < 18) {
+	saudacao = "Boa tarde";
+} else {
+	saudacao = "Boa noite";
 }
 
-// document.getElementById("texto").innerHTML = x;
+// document.getElementById("teste").innerHTML = `Olá, ${saudacao}`;
 
-// Declaração de variável com const
-const y = 5;
-
-document.getElementById("texto").innerHTML = y;
+const nome = "teste3";
+// nome = "teste2";
+// alert(nome);
+for (let i = 1; i <= 3; i++) {
+	// console.log("Coma 1 pedaço de pizza");
+	// alert(teste);
+	alert(nome);
+}
