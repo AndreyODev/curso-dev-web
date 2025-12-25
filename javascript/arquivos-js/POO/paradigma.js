63aa4ece15ceb45f590b53f2891d8a3cca5bbed3
Andrey Oliveira <andreyoliveira72005@gmail.com>
2025-12-25 20:50:01 -0300

    feat: adiciona a pasta POO e o arquivo paradigma

---
+++ javascript/arquivos-js/POO/paradigma.js
@@ -0,0 +1,27 @@
+// PARADIGMA PROCEDURAL (FUNÇÃO E PROCEDIMENTO)
+// function verificarDisponibilidade(quartos, ocupados) {
+// 	let resultado = quartos - ocupados;
+// 	console.log("Disponíveis: " + resultado);
+// }
+
+// verificarDisponibilidade(10, 4);
+
+// function verificarDisponibilidade(quartos, ocupados) {
+// 	return quartos - ocupados;
+// }
+// console.log("Disponíveis: " + verificarDisponibilidade(10, 4));
+
+// PARADIGMA ORIENTADO A OBJETOS
+// const hotel = {
+// 	nome: "Hotel Charmoso",
+// 	qtdQuartos: 0,
+// 	qtdQuartosOcupados: 0,
+// 	verificarDisponibilidade: function () {
+// 		return this.qtdQuartos - this.qtdQuartosOcupados;
+// 	},
+// };
+
+// hotel.qtdQuartos = 10;
+// hotel.qtdQuartosOcupados = 8;
+
+// console.log("Quantidade de quartos disponíveis: " + hotel.verificarDisponibilidade());
