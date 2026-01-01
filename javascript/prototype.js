9fbe340d77eaba15f0b35e7269a104fe6da7779a
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-01 20:20:44 -0300

    feat: adiciona o arquivo prototype

---
+++ javascript/prototype.js
@@ -0,0 +1,36 @@
+class Carro {
+	constructor() {
+		this.placa = "AMP-1230";
+	}
+}
+
+class Bmw extends Carro {
+	constructor() {
+		super();
+		this.nome = "BMW 320i";
+	}
+}
+
+// const objBmw = new Bmw();
+// console.log(objBmw.placa)
+// console.log(objBmw.nome)
+
+const veiculo = {
+	motor: "50 cavalos",
+};
+
+const carro = {
+	placa: "AMP-1230",
+	__proto__: veiculo,
+	acelerar() {
+		console.log("Acelerar");
+	},
+};
+
+const bmw = {
+	nome: "BMW 320i",
+	__proto__: carro,
+};
+console.log(bmw.placa);
+bmw.acelerar();
+bmw.motor;
