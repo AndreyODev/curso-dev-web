b7ecd6d4a9646aa5e6746a73cb776fc8781eee34
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-02 20:18:50 -0300

    feat: adiciona o arquivo método matemático

---
+++ javascript/funcoes_nativas/metodo_matematico.js
@@ -0,0 +1,12 @@
+// MATH
+let numero = 280.65;
+
+// Math.PI
+// Math.ceil(numero)
+// Math.floor(numero)
+// Math.round(numero)
+// Math.random() * 100
+// Math.min(15, 10)
+// Math.max(15, 10)
+// Math.pow(2, 10)
+console.log(Math.sqrt(9));
