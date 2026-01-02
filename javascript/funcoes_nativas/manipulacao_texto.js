0dcdedb8de13cbd3830a546c8343570e6c8a8b78
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-02 20:05:50 -0300

    feat: adiciona o arquivo manipulação de texto

---
+++ javascript/funcoes_nativas/manipulacao_texto.js
@@ -0,0 +1,43 @@
+// Manipulação de texto
+
+// let nome = "Andrey é gente boa"
+// nome.length
+// nome.charAt(5)
+// let n = nome.replace("Andrey", "Jefinho")
+// console.log(nome)
+// console.log(n)
+
+// let frase = "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo"
+// frase.substr(2, 7)
+
+// let f = frase.substr(0, 62) + "......."
+// console.log(f)
+
+// let nome = "Andrey Oliveira"
+// let resultado = nome.split(" ")
+// console.log(resultado[0])
+// console.log(resultado[1])
+
+// let nome = "andrey"
+// let sobrenome = "oliveira"
+// let s = sobrenome.slice(0, 4)
+
+// nome + s + "@gmail.com"
+
+// let pesquisa = "Andrey Oliveira"
+// let itemSalvo = "andrey oliveira"
+// pesquisa.toUpperCase()
+// let item = pesquisa.toLowerCase()
+// if (itemSalvo === item) {
+//   console.log("Achei")
+// }
+
+// let nome = "  Andrey Oliveira       "
+// nome
+// nome.trim()
+
+let a = "Olá";
+let b = "Andrey";
+let c = "Oliveira";
+
+console.log(a.concat(" ", b, " ", c));
