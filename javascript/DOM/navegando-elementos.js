cd943227a83053e12d0a0204db22d170974a116f
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-05 20:28:40 -0300

    feat: adiciona o arquivo navegando elementos

---
+++ javascript/DOM/navegando-elementos.js
@@ -0,0 +1,14 @@
+const lista = document.querySelector("#itens");
+const pai = document.querySelector("#pai");
+// console.log(lista.parentNode);
+// console.log(lista.childNodes[1]);
+// console.log(lista.firstChild);
+// console.log(lista.lastChild);
+// console.log(pai.nextSibling);
+console.log(pai.previousSibling);
+
+// console.log(lista.children);
+// console.log(lista.firstElementChild);
+// console.log(lista.lastElementChild);
+console.log(pai.nextElementSibling);
+console.log(pai.previousElementSibling);
