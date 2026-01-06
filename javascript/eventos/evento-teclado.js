c5ab860abf7daac5c4350f5c02c783488efab9e6
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-06 20:37:14 -0300

    feat: adiciona o arquivo evento de teclado

---
+++ javascript/eventos/evento-teclado.js
@@ -0,0 +1,13 @@
+const obj = document.getElementById("entrada");
+
+// obj.addEventListener("keydown", function () {
+// 	console.log("Executar");
+// });
+
+obj.addEventListener("keyup", function () {
+	console.log("Executar");
+});
+
+obj.addEventListener("keypress", function () {
+	console.log("Executar");
+});
