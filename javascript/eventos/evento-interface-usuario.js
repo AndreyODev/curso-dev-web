ab4888388da0262695ceca886648563553ba1504
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-06 20:11:33 -0300

    feat: adiciona o arquivo evento de interface do usuário

---
+++ javascript/eventos/evento-interface-usuario.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function () {
+	console.log("Executar");
+});
+
+window.addEventListener("error", function () {
+	console.log("Executar");
+});
+
+window.addEventListener("resize", function () {
+	console.log("Executar");
+});
+
+window.addEventListener("scroll", function () {
+	console.log("Executar");
+});
