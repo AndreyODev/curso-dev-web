d8155fda828e924497eda1214f046216fd969d5b
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-06 20:53:06 -0300

    feat: adiciona o arquivo evento de mouse

---
+++ javascript/eventos/evento-mouse.js
@@ -0,0 +1,34 @@
+const btn = document.getElementById("btn");
+const h1 = document.getElementById("h1");
+
+btn.addEventListener("click", function () {
+	console.log("Executar");
+});
+
+// h1.addEventListener("click", function () {
+// 	console.log("Executar h1");
+// });
+
+// h1.addEventListener("dblclick", function () {
+// 	console.log("Executar h1");
+// });
+
+// h1.addEventListener("mousedown", function () {
+// 	console.log("Executar h1");
+// });
+
+// h1.addEventListener("mouseup", function () {
+// 	console.log("Executar h1");
+// });
+
+// h1.addEventListener("mousemove", function () {
+// 	console.log("Executar h1");
+// });
+
+// h1.addEventListener("mouseover", function () {
+// 	console.log("Executar h1");
+// });
+
+h1.addEventListener("mouseout", function () {
+	console.log("Executar h1");
+});
