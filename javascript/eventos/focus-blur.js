0633661163fbec3d553a3d94e48cbc0630deb758
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-07 13:27:52 -0300

    feat: adiciona o arquivo focus-blur

---
+++ javascript/eventos/focus-blur.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("entrada");
+
+input.addEventListener("focus", function () {
+	console.log("Executar");
+});
+
+input.addEventListener("blur", function () {
+	console.log("Executar");
+});
