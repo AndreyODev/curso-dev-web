e52e6e4ac39efba2d762860b8b54acb0011829d3
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-23 21:30:56 -0300

    feat: adiciona o projeto App01 em React (CRA e Vite)

---
+++ react/vite/App1/src/App.jsx
@@ -0,0 +1,34 @@
+// import React from "react";
+import Foto from "./assets/img/eu2.jpeg";
+
+function App() {
+  const instagram = () => {
+    return "@andreyolliveira";
+  };
+  const github = "AndreyODev";
+  const curso = "Curso de React";
+  return (
+    <section>
+      {/* <p>AndreyODev</p>
+      <p>Curso de React</p> */}
+      {/* <p>GitHub: {github}</p> */}
+      <p>{"GitHub: " + github}</p>
+      <p>Instragram: {instagram()}</p>
+      <p>{curso}</p>
+      <img src={Foto} alt="Minha foto" />
+      <img src="./public/indian.png" alt="Moto" />
+    </section>
+  );
+}
+
+export default App;
+
+// import React from "react";
+
+// export default function App() {
+//   return (
+//     <>
+//       <p>AndreyODev</p>
+//     </>
+//   );
+// }
