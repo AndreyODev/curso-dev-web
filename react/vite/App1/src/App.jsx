eb2bcc441fb1e5772c3600832022392d7836771c
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-25 15:16:33 -0300

    feat: adiciona componentes Header e Corpo

--- react/vite/App1/src/App.jsx
+++ react/vite/App1/src/App.jsx
@@ -1,23 +1,31 @@
 // import React from "react";
-import Foto from "./assets/img/eu2.jpeg";
+// import Foto from "./assets/img/eu2.jpeg";
+
+import Header from "./components/Header.jsx";
+import Corpo from "./components/Corpo.jsx";
 
 function App() {
-  const instagram = () => {
-    return "@andreyolliveira";
-  };
-  const github = "AndreyODev";
-  const curso = "Curso de React";
+  // const instagram = () => {
+  //   return "@andreyolliveira";
+  // };
+  // const github = "AndreyODev";
+  // const curso = "Curso de React";
   return (
-    <section>
-      {/* <p>AndreyODev</p>
-      <p>Curso de React</p> */}
-      {/* <p>GitHub: {github}</p> */}
-      <p>{"GitHub: " + github}</p>
-      <p>Instragram: {instagram()}</p>
-      <p>{curso}</p>
-      <img src={Foto} alt="Minha foto" />
-      <img src="./public/indian.png" alt="Moto" />
-    </section>
+    // <section>
+    //   <p>AndreyODev</p>
+    //   <p>Curso de React</p>
+    //   <p>GitHub: {github}</p>
+    //   <p>{"GitHub: " + github}</p>
+    //   <p>Instragram: {instagram()}</p>
+    //   <p>{curso}</p>
+    //   <img src={Foto} alt="Minha foto" />
+    //   <img src="./public/indian.png" alt="Moto" />
+    // </section>
+
+    <>
+      <Header />
+      <Corpo />
+    </>
   );
 }
 
---
+++ react/vite/App1/src/components/Corpo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Corpo() {
+  return (
+    <section>
+      <h2>Curso de React</h2>
+      <p>Muito da hora a criação com o react</p>
+    </section>
+  );
+}
+export default Corpo;
