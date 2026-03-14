048936892f1c99af1d8ea0fedd6a694eaa603005
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-03-14 17:52:36 -0300

    feat: adiciona o método get

--- express/src/index.mjs
+++ express/src/index.mjs
@@ -4,6 +4,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (request, res) => {
+	// res.send("Hello, world");
+	// res.send({ msg: "Hello" });
+	res.status(201).send({ msg: "Hello" });
+});
+
+app.get("/api/users", (req, res) => {
+	res.send([
+		{ id: 1, nome: "Andrey", sobrenome: "Oliveira" },
+		{ id: 2, nome: "Jefinho", sobrenome: "Dias" },
+		{ id: 3, nome: "Gabryella", sobrenome: "Faria" },
+	]);
+});
+
+app.get("/api/produtos", (request, res) => {
+	res.send([{ id: 123, nome: "File de peito", price: 16 }]);
+});
+
 app.listen(PORT, () => {
 	console.log(`Execuntando na porta ${PORT}`);
 });
