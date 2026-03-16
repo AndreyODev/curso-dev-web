8c24580056388d4e6da7190b4149ddfa2d231ab7
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-03-16 16:28:15 -0300

    feat: adiciona parâmentro de rota

--- express/src/index.mjs
+++ express/src/index.mjs
@@ -10,12 +10,38 @@ app.get("/", (request, res) => {
 	res.status(201).send({ msg: "Hello" });
 });
 
+// app.get("/api/users", (req, res) => {
+// 	res.send([
+// 		{ id: 1, nome: "Andrey", sobrenome: "Oliveira" },
+// 		{ id: 2, nome: "Jefinho", sobrenome: "Dias" },
+// 		{ id: 3, nome: "Gabryella", sobrenome: "Faria" },
+// 	]);
+// });
+
+const mockUsers = [
+	{ id: 1, nome: "Andrey", sobrenome: "Oliveira" },
+	{ id: 2, nome: "Jefinho", sobrenome: "Dias" },
+	{ id: 3, nome: "Gabryella", sobrenome: "Faria" },
+];
+
 app.get("/api/users", (req, res) => {
-	res.send([
-		{ id: 1, nome: "Andrey", sobrenome: "Oliveira" },
-		{ id: 2, nome: "Jefinho", sobrenome: "Dias" },
-		{ id: 3, nome: "Gabryella", sobrenome: "Faria" },
-	]);
+	res.send(mockUsers);
+});
+
+app.get("/api/users/:id", (req, res) => {
+	// console.log(parseInt(req.params.id));
+	const parsedId = parseInt(req.params.id);
+	console.log(parsedId);
+
+	const findUser = mockUsers.find((user) => {
+		return user.id === parsedId;
+	});
+
+	if (!findUser) {
+		return res.status(404).send("Usuário não encontrado");
+	}
+
+	res.send(findUser);
 });
 
 app.get("/api/produtos", (request, res) => {
