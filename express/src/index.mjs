0fbc23621768ca8ba284873f830fe454b51cb927
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-03-19 14:50:55 -0300

    feat: adiciona parâmetro de consulta

--- express/src/index.mjs
+++ express/src/index.mjs
@@ -19,15 +19,34 @@ app.get("/", (request, res) => {
 // });
 
 const mockUsers = [
-	{ id: 1, nome: "Andrey", sobrenome: "Oliveira" },
-	{ id: 2, nome: "Jefinho", sobrenome: "Dias" },
-	{ id: 3, nome: "Gabryella", sobrenome: "Faria" },
+	{ id: 1, nome: "andrey", sobrenome: "Oliveira" },
+	{ id: 2, nome: "andrey", sobrenome: "Oliveira" },
+	{ id: 3, nome: "jefinho", sobrenome: "Dias" },
+	{ id: 4, nome: "gabryella", sobrenome: "Faria" },
 ];
 
 app.get("/api/users", (req, res) => {
-	res.send(mockUsers);
+	console.log(req.query);
+	// const { query: { filter, value },} = req;
+	const {
+		query: { filter, value },
+	} = req;
+
+	// Quando o filtro e o valor forem underfined
+	if (!filter && !value) {
+		return res.send(mockUsers);
+	}
+
+	if (filter && value) {
+		return res.send(
+			mockUsers.filter((user) => {
+				return user[filter].includes(value);
+			}),
+		);
+	}
 });
 
+// Parâmetro de rota
 app.get("/api/users/:id", (req, res) => {
 	// console.log(parseInt(req.params.id));
 	const parsedId = parseInt(req.params.id);
@@ -43,6 +62,38 @@ app.get("/api/users/:id", (req, res) => {
 
 	res.send(findUser);
 });
+
+// Parâmetro de consulta pelo id usando o find
+// app.get("/api/users", (req, res) => {
+// 	// console.log(parseInt(req.params.id));
+// 	const parsedId = parseInt(req.query.id);
+// 	console.log(parsedId);
+
+// 	const findUser = mockUsers.find((user) => {
+// 		return user.id === parsedId;
+// 	});
+
+// 	if (!findUser) {
+// 		return res.status(404).send("Usuário não encontrado");
+// 	}
+
+// 	res.send(findUser);
+// });
+
+// Parâmetro de consulta pelo nome
+// app.get("/api/users", (req, res) => {
+// 	const nome = req.query.nome;
+
+// 	const filtrerUser = mockUsers.filter((user) => {
+// 		return user.nome === nome;
+// 	});
+
+// 	if (filtrerUser.length === 0) {
+// 		return res.status(404).send("Usuário não encontrado");
+// 	}
+
+// 	res.send(filtrerUser);
+// });
 
 app.get("/api/produtos", (request, res) => {
 	res.send([{ id: 123, nome: "File de peito", price: 16 }]);
